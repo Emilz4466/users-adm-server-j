4c3f94df8a42ec5197ccbbdaf8fdea4a3c197260
Emilz4466 <emil.z4466@gmail.com>
2023-09-16 22:43:55 +0200

    init commit

---
+++ src/main/java/pl/com/emilz/usersadmserverj/UsersAdmServerJApplication.java
@@ -0,0 +1,13 @@
+package pl.com.emilz.usersadmserverj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAdmServerJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersAdmServerJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/emilz/usersadmserverj/UsersAdmServerJApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.emilz.usersadmserverj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAdmServerJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
